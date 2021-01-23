b5cd5d189984c526dde1e0297a0ac0c97615f302
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-01-23 12:33:57 -0300

    show methods in pets added

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -14,5 +14,40 @@ class Pet < ApplicationRecord
     self.client.name
   end
 
+  def average_weight
+    weight_array = PetHistory.where(pet_id: id).pluck(:weight)
+    if weight_array.size == 0
+        "This pet don't have histories"
+    else
+        weight_array.sum / weight_array.size
+    end
+  end
+
+  def average_height
+      height_array = PetHistory.where(pet_id: id).pluck(:height)
+      if height_array.size == 0
+          "This pet don't have histories"
+      else
+          height_array.sum / height_array.size
+      end
+  end
+
+  def max_weight
+      weight_array = PetHistory.where(pet_id: id).pluck(:weight)
+      if weight_array.any?
+          weight_array.max
+      else
+          "This pet don't have histories"
+      end
+  end
+
+  def max_height
+      height_array = PetHistory.where(pet_id: id).pluck(:height)
+      if height_array.any?
+          height_array.max
+      else
+          "This pet don't have histories"
+      end
+  end
 
 end
